0acf7967d171998d9296f6264f4a2359e43d4e91
unknown <pranavghoderao@gmail.com>
2020-05-27 18:28:57 +0530

    commit 1

--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -81,8 +81,8 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-"""
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -93,6 +93,7 @@ DATABASES = {
         'PORT': '3306',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
